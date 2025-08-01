8ee215e312a03d0b76b23f229da93a318619e343
jhlee <jhlee89898989@gmail.com>
2025-08-02 00:29:27 +0900

    Replace thumbnail replaced

--- src/app/_components/novel-panels/novel-panels.tsx
+++ src/app/_components/novel-panels/novel-panels.tsx
@@ -1,5 +1,8 @@
+import { Image } from '@heroui/react';
+import Link from 'next/link';
+
 interface Props {
-	items: number[];
+	items: string[];
 	title: string;
 }
 export default function NovelPanels({ title, items }: Props) {
@@ -7,20 +10,20 @@ export default function NovelPanels({ title, items }: Props) {
 		<section>
 			<div className='text-lg font-bold mt-4 mb-2'>{title}</div>
 			<div className='grid grid-cols-6 gap-2'>
-				{items.map((n, i) => (
-					<div
-						key={n + '-' + i}
-						className={`flex items-end aspect-[1/2] rounded-lg bg-slate-${
-							i + 1
-						}00 `}
-					>
-						<div className='h-1/2 w-full flex flex-col rounded-b-lg justify-end pb-2 bg-gradient-to-t from-gray-700 to-transparent'>
-							<p className='text-center font-bold text-white'>
-								{n}. 제목은 여기에
-							</p>
+				{items.map((n) => (
+					<Link href='' key={n} className='relative'>
+						<Image
+							className='object-cover aspect-[1/2]'
+							alt='book cover image'
+							radius='sm'
+							src={n}
+							isZoomed
+						/>
+						<div className='absolute bottom-0 z-10 h-1/2 w-full flex flex-col rounded-b-lg justify-end pb-2 bg-gradient-to-t from-gray-700 to-transparent'>
+							<p className='text-center font-bold text-white'>제목은 여기에</p>
 							<small className='text-center text-slate-100'>조희수</small>
 						</div>
-					</div>
+					</Link>
 				))}
 			</div>
 		</section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,39 @@ import MillionPagesNovels from './_components/million-pages-novels';
 import RecentEvents from './_components/recent-events';
 import NovelPanels from './_components/novel-panels/novel-panels';
 
+const sample1 = [
+	'/cover_thumbnails/novel_cover_thumbnail_22.png',
+	'/cover_thumbnails/novel_cover_thumbnail_23.png',
+	'/cover_thumbnails/novel_cover_thumbnail_24.png',
+	'/cover_thumbnails/novel_cover_thumbnail_25.png',
+	'/cover_thumbnails/novel_cover_thumbnail_26.png',
+	'/cover_thumbnails/novel_cover_thumbnail_27.png',
+];
+const sample2 = [
+	'/cover_thumbnails/novel_cover_thumbnail_28.png',
+	'/cover_thumbnails/novel_cover_thumbnail_29.png',
+	'/cover_thumbnails/novel_cover_thumbnail_30.png',
+	'/cover_thumbnails/novel_cover_thumbnail_31.png',
+	'/cover_thumbnails/novel_cover_thumbnail_32.png',
+	'/cover_thumbnails/novel_cover_thumbnail_1.png',
+];
+const sample3 = [
+	'/cover_thumbnails/novel_cover_thumbnail_2.png',
+	'/cover_thumbnails/novel_cover_thumbnail_3.png',
+	'/cover_thumbnails/novel_cover_thumbnail_4.png',
+	'/cover_thumbnails/novel_cover_thumbnail_5.png',
+	'/cover_thumbnails/novel_cover_thumbnail_6.png',
+	'/cover_thumbnails/novel_cover_thumbnail_7.png',
+];
+const sample4 = [
+	'/cover_thumbnails/novel_cover_thumbnail_8.png',
+	'/cover_thumbnails/novel_cover_thumbnail_9.png',
+	'/cover_thumbnails/novel_cover_thumbnail_10.png',
+	'/cover_thumbnails/novel_cover_thumbnail_11.png',
+	'/cover_thumbnails/novel_cover_thumbnail_12.png',
+	'/cover_thumbnails/novel_cover_thumbnail_13.png',
+];
+
 export default function Home() {
 	return (
 		<section className='w-full flex flex-col gap-2'>
@@ -11,10 +44,10 @@ export default function Home() {
 			<RecentEvents />
 			<RecentTop6Novels />
 			<MillionPagesNovels />
-			<NovelPanels title='무료 웹소설' items={[1, 2, 3, 4, 5, 6]} />
-			<NovelPanels title='스포츠 웹소설' items={[1, 2, 3, 4, 5, 6]} />
-			<NovelPanels title='로맨스 웹소설' items={[1, 2, 3, 4, 5, 6]} />
-			<NovelPanels title='성인 웹소설' items={[1, 2, 3, 4, 5, 6]} />
+			<NovelPanels title='무료 웹소설' items={sample1} />
+			<NovelPanels title='스포츠 웹소설' items={sample2} />
+			<NovelPanels title='로맨스 웹소설' items={sample3} />
+			<NovelPanels title='성인 웹소설' items={sample4} />
 		</section>
 	);
 }
