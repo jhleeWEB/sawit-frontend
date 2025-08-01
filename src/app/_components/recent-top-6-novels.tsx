ded5a4c63c2595e64215c03dcc6f2991ef790e79
jhlee <jhlee89898989@gmail.com>
2025-08-02 00:11:54 +0900

    Replace thumbnmail replaced

--- src/app/_components/recent-top-6-novels.tsx
+++ src/app/_components/recent-top-6-novels.tsx
@@ -1,15 +1,29 @@
-const novels = [1, 2, 3, 4, 5, 6];
+import { Image } from '@heroui/react';
+import Link from 'next/link';
+
+const novels = [
+	'/cover_thumbnails/novel_cover_thumbnail_4.png',
+	'/cover_thumbnails/novel_cover_thumbnail_8.png',
+	'/cover_thumbnails/novel_cover_thumbnail_20.png',
+	'/cover_thumbnails/novel_cover_thumbnail_11.png',
+	'/cover_thumbnails/novel_cover_thumbnail_12.png',
+	'/cover_thumbnails/novel_cover_thumbnail_21.png',
+];
 
 export default function RecentTop6Novels() {
 	return (
 		<div className='grid grid-cols-6 gap-2'>
-			{novels.map((novel, i) => (
-				<div
-					key={novel + '-' + i}
-					className={`col-span-1 h-[230px] rounded-lg bg-slate-${i + 1}00`}
-				>
-					{novel}
-				</div>
+			{novels.map((novel) => (
+				<Link key={novel} href={''}>
+					<Image
+						className='object-cover'
+						alt='book cover image'
+						height={230}
+						radius='sm'
+						src={novel}
+						isZoomed
+					/>
+				</Link>
 			))}
 		</div>
 	);
