8c6037a8c06be5583e1a5051bcaf98e564b48c77
jhlee <jhlee89898989@gmail.com>
2025-08-02 18:37:43 +0900

    Add controller added

--- src/app/a/[author]/publish/_components/editor.tsx
+++ src/app/a/[author]/publish/_components/editor.tsx
@@ -1,10 +1,10 @@
 'use client';
-import { Input } from '@heroui/react';
+import { Button, Input } from '@heroui/react';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
 export default function EditorWrapper() {
 	return (
-		<section className='w-full h-dvh'>
+		<section className='relative w-full h-dvh'>
 			<div className='bg-slate-50 rounded-lg mt-16 p-4'>
 				<Input
 					label='제목'
@@ -18,6 +18,15 @@ export default function EditorWrapper() {
 				/>
 
 				<Editor height='600px' initialEditType='wysiwyg' />
+			</div>
+			<div className='absolute gap-2 flex flex-col'>
+				<Button variant='flat' color='default'>
+					임시저장
+				</Button>
+				<Button variant='bordered'>미리보기</Button>
+				<Button variant='flat' color='primary'>
+					업로드
+				</Button>
 			</div>
 		</section>
 	);
