84e92e8b128e642f28d0969e81e08b98eb13065b
jhlee <jhlee89898989@gmail.com>
2025-08-06 18:59:11 +0900

    Cleanup delete unused codes

--- src/components/Title/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Title(props: React.PropsWithChildren) {
-	return (
-		<h1 className='mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl'>
-			{props.children}
-		</h1>
-	);
-}
