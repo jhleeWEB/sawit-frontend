3ccd9197e71cc0c9724fbbfb53132935ce884111
jhlee <jhlee89898989@gmail.com>
2025-08-06 19:48:35 +0900

    Update novel samples added

--- src/app/(main)/_components/novel-panels/novel-panels.tsx
+++ src/app/(main)/_components/novel-panels/novel-panels.tsx
@@ -1,27 +1,40 @@
 import { Image } from '@heroui/react';
 import Link from 'next/link';
+import { NovelSample } from '../../a/[author]/page';
+import { IoEyeOutline } from 'react-icons/io5';
+import { formatToKoreanUnits } from '@/utils/format-to-korean-unit';
 
 interface Props {
-	items: string[];
+	images: string[]; //temporary image src
+	novels: NovelSample[];
 	title: string;
 }
-export default function NovelPanels({ title, items }: Props) {
+export default function NovelPanels({ title, images, novels }: Props) {
 	return (
 		<section>
 			<div className='text-lg font-bold mt-4 mb-2'>{title}</div>
 			<div className='grid grid-cols-6 gap-2'>
-				{items.map((n) => (
-					<Link href='' key={n} className='relative'>
+				{novels.map((novel, i) => (
+					<Link
+						href={`/a/${novel.author}/${novel.id}`}
+						key={novel.title}
+						className='relative'
+					>
 						<Image
 							className='object-cover aspect-[1/2]'
 							alt='book cover image'
 							radius='sm'
-							src={n}
+							src={images[i]}
 							isZoomed
 						/>
 						<div className='absolute bottom-0 z-10 h-1/2 w-full flex flex-col rounded-b-lg justify-end pb-2 bg-gradient-to-t from-gray-700 to-transparent'>
-							<p className='text-center font-bold text-white'>제목은 여기에</p>
-							<small className='text-center text-slate-100'>조희수</small>
+							<p className='text-center font-bold text-white'>{novel.title}</p>
+							<div className='flex gap-1 items-center justify-center  text-slate-100'>
+								<IoEyeOutline />
+								<small className='text-center'>
+									{formatToKoreanUnits(novel.stats.views)}
+								</small>
+							</div>
 						</div>
 					</Link>
 				))}
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,6 +3,7 @@ import RecentTop6Novels from './_components/recent-top-6-novels';
 import MillionPagesNovels from './_components/million-pages-novels';
 import RecentEvents from './_components/recent-events';
 import NovelPanels from './_components/novel-panels/novel-panels';
+import { NovelSample } from './a/[author]/page';
 
 const sample1 = [
 	'/cover_thumbnails/novel_cover_thumbnail_22.png',
@@ -37,17 +38,56 @@ const sample4 = [
 	'/cover_thumbnails/novel_cover_thumbnail_13.png',
 ];
 
-export default function Home() {
+export default async function Home() {
+	const freeNovelData = fetchTopFreeNovels();
+	const sportsNovelData = fetchTopSportsNovels();
+	const romanceNovelData = fetchTopRomanceNovels();
+	const adultNovelData = fetchTopAdultNovels();
+	const [freeNovels, sportsNovels, romanceNovels, adultNovels] =
+		await Promise.all([
+			freeNovelData,
+			sportsNovelData,
+			romanceNovelData,
+			adultNovelData,
+		]);
 	return (
 		<section className='w-full flex flex-col gap-2'>
 			<ChipShortcuts />
 			<RecentEvents />
 			<RecentTop6Novels />
 			<MillionPagesNovels />
-			<NovelPanels title='무료 웹소설' items={sample1} />
-			<NovelPanels title='스포츠 웹소설' items={sample2} />
-			<NovelPanels title='로맨스 웹소설' items={sample3} />
-			<NovelPanels title='성인 웹소설' items={sample4} />
+			<NovelPanels title='무료 웹소설' novels={freeNovels} images={sample1} />
+			<NovelPanels
+				title='스포츠 웹소설'
+				novels={sportsNovels}
+				images={sample2}
+			/>
+			<NovelPanels
+				title='로맨스 웹소설'
+				novels={romanceNovels}
+				images={sample3}
+			/>
+			<NovelPanels title='성인 웹소설' novels={adultNovels} images={sample4} />
 		</section>
 	);
+}
+
+async function fetchTopFreeNovels(): Promise<NovelSample[]> {
+	const data = await import('/public/samples/korean_sample_novels_100.json');
+	return (Array.from(data) as NovelSample[]).slice(0, 6);
+}
+
+async function fetchTopSportsNovels(): Promise<NovelSample[]> {
+	const data = await import('/public/samples/korean_sample_novels_100.json');
+	return (Array.from(data) as NovelSample[]).slice(7, 13);
+}
+
+async function fetchTopRomanceNovels(): Promise<NovelSample[]> {
+	const data = await import('/public/samples/korean_sample_novels_100.json');
+	return (Array.from(data) as NovelSample[]).slice(13, 19);
+}
+
+async function fetchTopAdultNovels(): Promise<NovelSample[]> {
+	const data = await import('/public/samples/korean_sample_novels_100.json');
+	return (Array.from(data) as NovelSample[]).slice(19, 25);
 }
