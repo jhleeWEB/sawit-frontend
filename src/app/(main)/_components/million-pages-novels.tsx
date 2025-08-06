efe73b79304a06a014d19ff92f76a9265c47c226
jhlee <jhlee89898989@gmail.com>
2025-08-06 18:57:13 +0900

    Update tooltip added

--- src/app/(main)/_components/million-pages-novels.tsx
+++ src/app/(main)/_components/million-pages-novels.tsx
@@ -1,4 +1,6 @@
-import { Image } from '@heroui/react';
+import { Image, Tooltip } from '@heroui/react';
+import { NovelSample } from '../a/[author]/page';
+import Link from 'next/link';
 
 const uris = [
 	'/cover_thumbnails/novel_cover_thumbnail_14.png',
@@ -11,54 +13,79 @@ const uris = [
 	'/cover_thumbnails/novel_cover_thumbnail_21.png',
 ];
 
-export default function MillionPagesNovels() {
+export default async function MillionPagesNovels() {
+	const novelDatas = await fetchNovelDatas();
+
 	return (
 		<div>
 			<h1 className='font-bold text-lg mt-4 mb-2'>밀리언 페이지</h1>
 			<div className='overflow-hidden rounded-lg'>
 				<div className='flex gap-2 group'>
 					<section className='flex gap-2 animate-slide group-hover:pause'>
-						{uris.map((n, i) => (
-							<div key={n + '-' + i} className='flex flex-col gap-2'>
+						{novelDatas.map((novel, i) => (
+							<Link
+								key={novel.title + '-' + i}
+								href={`/a/${novel.author}/${novel.id}`}
+								className='flex flex-col gap-2'
+							>
 								<Image
 									className='object-cover min-w-[90px]'
 									alt='book cover image'
 									height={120}
 									radius='sm'
-									src={n}
+									src={uris[i]}
 									isZoomed
 								/>
-								<small className='text-xs'>제목 여기에 넣기</small>
-								<small className='text-[10px] text-slate-600'>
-									장르는 여기에
-								</small>
-							</div>
+								<Tooltip showArrow content={<p>{novel.title}</p>}>
+									<small className='w-[90px] text-xs text-nowrap overflow-hidden text-ellipsis'>
+										{novel.title}
+									</small>
+								</Tooltip>
+								<div className='flex gap-1'>
+									{novel.about.category.map((n) => (
+										<small key={n} className='text-[10px] text-slate-600'>
+											{n}
+										</small>
+									))}
+								</div>
+							</Link>
 						))}
 					</section>
-					<section
-						aria-hidden
-						className='flex gap-2 animate-slide group-hover:pause'
-					>
-						{uris.map((n, i) => (
-							<div key={n + '-' + i}>
+					<section className='flex gap-2 animate-slide group-hover:pause'>
+						{novelDatas.map((novel, i) => (
+							<Link
+								key={novel.title + '-' + i}
+								href={`/a/${novel.author}/${novel.id}`}
+								className='flex flex-col gap-2'
+							>
 								<Image
 									className='object-cover min-w-[90px]'
 									alt='book cover image'
-									width={90}
 									height={120}
 									radius='sm'
-									src={n}
+									src={uris[i]}
 									isZoomed
 								/>
-								<small className='text-xs'>제목 여기에 넣기</small>
-								<small className='text-[10px] text-slate-600'>
-									장르는 여기에
+								<small className='w-[90px] text-xs text-nowrap overflow-hidden text-ellipsis'>
+									{novel.title}
 								</small>
-							</div>
+								<div className='flex gap-1'>
+									{novel.about.category.map((n) => (
+										<small key={n} className='text-[10px] text-slate-600'>
+											{n}
+										</small>
+									))}
+								</div>
+							</Link>
 						))}
 					</section>
 				</div>
 			</div>
 		</div>
 	);
+}
+
+async function fetchNovelDatas(): Promise<NovelSample[]> {
+	const data = await import('/public/samples/korean_sample_novels_100.json');
+	return (Array.from(data) as NovelSample[]).slice(0, 8);
 }
