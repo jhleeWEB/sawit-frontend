890d94cc588b92fee75f62166a36b27be604d7ff
jhlee <jhlee89898989@gmail.com>
2025-09-12 09:23:26 +0900

    Add 댓글 추가 시 임시 댓글 표기 기능 추가

--- src/features/post-card/_components/post-comment-section.tsx
+++ src/features/post-card/_components/post-comment-section.tsx
@@ -1,12 +1,9 @@
 "use client";
 
+import { useMemo } from "react";
 import { useQuery } from "@tanstack/react-query";
-import Header from "./header";
 import fetchComments, { Comment } from "@/service/fetch_comments";
-import CommentActionRow from "./comment-action-row";
-import { memo, useMemo, useState } from "react";
-import { PiPlusCircle, PiMinusCircle } from "react-icons/pi";
-import { Button } from "@heroui/react";
+import { CommentNode, MemoComment } from "./memo-comment";
 
 interface Props {
   postId: number;
@@ -27,63 +24,6 @@ export default function PostCommentSection({ postId }: Props) {
     </section>
   );
 }
-
-function CommentComponent({ commentNode }: { commentNode: CommentNode }) {
-  const [toggleReply, setToggleReply] = useState(true);
-  const {
-    depth,
-
-    comment,
-    owner_icon,
-    owner_username,
-    created_at,
-    children,
-  } = commentNode;
-  const hasChildren = children.length > 0;
-  const indent = depth * 40;
-  return (
-    <>
-      <article style={{ marginLeft: `${indent}px` }}>
-        <div className="flex">
-          <Header
-            icon={owner_icon}
-            name={owner_username}
-            created_at={created_at}
-          />
-          {hasChildren && (
-            <Button
-              isIconOnly
-              variant="light"
-              className="text-neutral-500"
-              radius="full"
-              startContent={
-                toggleReply ? (
-                  <PiMinusCircle size={20} />
-                ) : (
-                  <PiPlusCircle size={20} />
-                )
-              }
-              onPress={() => setToggleReply((prev) => !prev)}
-            />
-          )}
-        </div>
-        <div className="flex flex-col ml-10">
-          <p>{comment}</p>
-          <CommentActionRow comment={commentNode} />
-        </div>
-      </article>
-      {hasChildren &&
-        toggleReply &&
-        children.map((reply) => (
-          <MemoComment key={reply.id} commentNode={reply} />
-        ))}
-    </>
-  );
-}
-
-const MemoComment = memo(CommentComponent);
-
-export type CommentNode = Comment & { children: CommentNode[] };
 
 export function buildTree(rows: Comment[]): CommentNode[] {
   const byId = new Map<number, CommentNode>();
