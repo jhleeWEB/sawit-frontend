7700360473777716aa48389963604a0df38f5f00
jhlee <jhlee89898989@gmail.com>
2025-10-16 11:56:31 +0900

    Add mobile update profile button added

--- src/app/(main)/u/[username]/_components/user-header.tsx
+++ src/app/(main)/u/[username]/_components/user-header.tsx
@@ -1,17 +1,18 @@
 import { IDENTITY } from "@/constants/identifiers";
 import fetchUser from "@/service/fetch-user";
-import { Avatar } from "@heroui/react";
+import { Avatar, Button } from "@heroui/react";
+import Link from "next/link";
 
 export default async function UserHeader({ username }: { username: string }) {
   const userInfo = await fetchUser({ username });
   if (!userInfo) return;
   return (
-    <div className="flex col-start-1 col-span-2 w-full p-4">
+    <div className="col-span-2 col-start-1 flex w-full py-4">
       <Avatar
         alt="icon"
         radius="full"
         src={userInfo.image}
-        className="w-[60px] h-[60px] shrink-0 mr-2"
+        className="mr-2 h-[60px] w-[60px] shrink-0"
       />
       <div className="flex flex-col">
         <h1 className="text-3xl font-bold">{username}</h1>
@@ -20,6 +21,15 @@ export default async function UserHeader({ username }: { username: string }) {
           {username}
         </p>
       </div>
+      <Button
+        radius="full"
+        size="sm"
+        as={Link}
+        href={`/settings/account`}
+        className="ml-auto sm:hidden"
+      >
+        업데이트
+      </Button>
     </div>
   );
 }
