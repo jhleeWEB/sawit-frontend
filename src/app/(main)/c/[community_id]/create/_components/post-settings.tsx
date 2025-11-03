eefb0442b834c0af19e3fd12386584d0056ab7ac
jhlee <jhlee89898989@gmail.com>
2025-11-03 13:59:28 +0900

    Add 게시물 공개 설정 추가

---
+++ src/app/(main)/c/[community_id]/create/_components/post-settings.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { Divider, Switch } from "@heroui/react";
+import { useState } from "react";
+
+export function PostSettings() {
+  const [isPrivate, setIsPrivate] = useState(false);
+  const [isSpoiler, setIsSpoiler] = useState(false);
+  const [isNFSW, setIsNFSW] = useState(false);
+  return (
+    <section className="flex flex-col gap-2 rounded-xl bg-slate-50 p-4">
+      <h1 className="text-lg font-bold">노출 설정</h1>
+      <div className="flex w-full justify-between">
+        <h1>노출</h1>
+        <Switch
+          className="flex-inline flex-row-reverse gap-2"
+          size="sm"
+          isSelected={isPrivate}
+          onValueChange={setIsPrivate}
+        >
+          {isPrivate ? "비공개" : "공개"}
+        </Switch>
+      </div>
+      <div className="flex w-full justify-between">
+        <h1>스포일러</h1>
+        <Switch
+          className="flex-inline flex-row-reverse gap-2"
+          size="sm"
+          isSelected={isSpoiler}
+          onValueChange={setIsSpoiler}
+        >
+          {isSpoiler ? "스포" : "일반"}
+        </Switch>
+      </div>
+      <div className="flex w-full justify-between">
+        <h1>후방주의</h1>
+        <Switch
+          className="flex-inline flex-row-reverse gap-2"
+          size="sm"
+          isSelected={isNFSW}
+          onValueChange={setIsNFSW}
+        >
+          {isNFSW ? "주의" : "비주의"}
+        </Switch>
+      </div>
+      <Divider />
+      <div></div>
+    </section>
+  );
+}
--- src/app/(main)/create-post/page.tsx
+++ src/app/(main)/create-post/page.tsx
@@ -1,16 +1,19 @@
 import PostFormProvider from "../c/[community_id]/create/_components/form-provider";
 import PostForm from "../c/[community_id]/create/_components/post-form";
+import { PostSettings } from "../c/[community_id]/create/_components/post-settings";
 
 export default async function CreatePostPage() {
   return (
     <div className="main-container">
-      <main className="w-full p-2">
-        <h1 className="mb-4 text-2xl font-bold">게시물 만들기</h1>
-        <PostFormProvider>
+      <PostFormProvider>
+        <main className="w-full">
+          <h1 className="mb-4 text-2xl font-bold">게시물 만들기</h1>
           <PostForm />
-        </PostFormProvider>
-      </main>
-      {/* <div className="right-menu-container"></div> */}
+        </main>
+        <div className="right-menu-container">
+          <PostSettings />
+        </div>
+      </PostFormProvider>
     </div>
   );
 }
